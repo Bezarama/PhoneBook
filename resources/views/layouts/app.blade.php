89786bf1010047ccb4ca620ef52a4d04efabc2ad
Denis Zinoviev <bezarama@gmail.com>
2022-05-24 19:06:34 +0300

    Вебинтерфейс. В меню для ссылки на документацию Swagger-а прописано target="_blank".

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,7 +48,7 @@
                             </a>
                             <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownAPI">
                                 <a class="dropdown-item" href="{{ route('token.index') }}">Управление токенами</a>
-                                <a class="dropdown-item" href="{{ url('/api/documentation') }}">Документация Swagger</a>
+                                <a class="dropdown-item" target="_blank" href="{{ url('/api/documentation') }}">Документация Swagger</a>
                             </div>
                         </li>
                     </ul>
